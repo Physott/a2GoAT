9e81c9ab7fcd1d670ad6684b201687db2bb56387
Patrik Ott <ott@kph.uni-mainz.de>
2014-11-25 00:28:03 +0100

    personel fitter.

--- inc/GKinFitter.h
+++ inc/GKinFitter.h
@@ -28,7 +28,7 @@ class GKinFitter
 	TMatrixD fmlamda;  	//Vector of lagrangian multipliers
 	TMatrixD fmV_D;    	//Covariance matrix of constraints (TO BE INVERTED)
 	Double_t fchi2;
-	TMatrixD fT;     	//Overall transforamtin matrix from Spherical->Cart
+    TMatrixD fT;     	//Overall transforamtin matrix from Spherical->Cart
 	TLorentzVector fPtot;
 
 public:
@@ -61,6 +61,87 @@ public:
 	void ResetMatrices();
     void Reset()            {ResetConstraints();ResetParticles();ResetMatrices();}
 	void Debug();
+};
+
+
+#define GMyTrackM_CBRadius  1
+#define GMyTrackM_nParameters  6
+#define GMyTrackH_nParameters  7
+#define GMyTrackW_nParameters  7
+#define GMyKinFitter_fNvar  7
+#define GMyKinFitter_l      1
+
+class GMyTrackM
+{
+private:
+    Double_t    e;
+    Double_t    theta;
+    Double_t    phi;
+    Double_t    mass;
+    TVector3    vertex;
+    Double_t    de;
+    Double_t    dtheta;
+    Double_t    dphi;
+    TVector3    dvertex;
+
+public:
+    GMyTrackM(const Double_t E, const Double_t Theta, const Double_t Phi, const Double_t Mass, const TVector3& Vertex, const Double_t DE, const Double_t DTheta, const Double_t DPhi, const TVector3& DVertex);
+    ~GMyTrackM();
+
+    TMatrixD    GetParametersH()    const;
+    TMatrixD    GetCovarianceH()    const;
+};
+
+
+class GMyTrackH
+{
+private:
+    TVector3    helpVertex;
+    Double_t    e;
+    Double_t    mass;
+    TVector3    vertex;
+    TMatrixD    cm;
+
+public:
+    GMyTrackH(const GMyTrackM& m);
+    ~GMyTrackH();
+
+    TMatrixD    GetParametersW()    const;
+    TMatrixD    GetCovarianceW()    const;
+};
+
+
+
+class   GMyKinFitter
+{
+private:
+    Int_t nPart; //Number of particles
+    Int_t nPar; //Number of parameters=Npart*fNvar
+    Int_t nCon; //Number of constraints
+    Int_t fNparti; //count Number of particles added
+    Int_t fNpari; //count Number of particles added
+    Int_t fNconi; // countNumber of constraints added
+    Int_t fNiter; // Number of times Solve has been called
+    TMatrixD fmAlpha0;	//original parameters
+    TMatrixD fmAlpha;  	//fitted parameters
+    TMatrixD fmV_Alpha0;//Covariance matrix for original parameters
+    TMatrixD fmV_Alpha; //Covariance matrix for fitted parameters
+    TMatrixD fmD;      	//Matrix of constraint derivitives
+    TMatrixD fmd;      	//Vector of evaluated constraints
+    TMatrixD fmlamda;  	//Vector of lagrangian multipliers
+    TMatrixD fmV_D;    	//Covariance matrix of constraints (TO BE INVERTED)
+    TMatrixD fT;     	//Overall transforamtin matrix from Spherical->Cart
+    TLorentzVector fPtot;
+protected:
+    static  TMatrixD    ConvertMtoH(const Double_t* pIn, const Double_t* dpIn, Double_t* pOut);
+    static  TMatrixD    ConvertHtoW(const Double_t* pIn, const TMatrixD& dp, const Double_t mass, Double_t *pOut);
+
+public:
+    GMyKinFitter(const Int_t npart, const Int_t ncon);
+    ~GMyKinFitter();
+
+    void    AddParticle(const Double_t* p, const Double_t* dp);
+    void    Reset();
 };
 
 #endif
--- src/GKinFitter.cc
+++ src/GKinFitter.cc
@@ -372,3 +372,445 @@ void GKinFitter::Debug(){
   mCheck1.Print();
 
 }
+
+
+
+
+
+
+
+GMyTrackM::GMyTrackM(const Double_t E, const Double_t Theta, const Double_t Phi, const Double_t Mass, const TVector3 &Vertex, const Double_t DE, const Double_t DTheta, const Double_t DPhi, const TVector3 &DVertex):
+    e(E),
+    theta(Theta),
+    phi(Phi),
+    mass(Mass),
+    vertex(Vertex),
+    de(DE),
+    dtheta(DTheta),
+    dphi(DPhi),
+    dvertex(DVertex)
+{
+
+}
+
+GMyTrackM::~GMyTrackM()
+{
+}
+
+TMatrixD    GMyTrackM::GetParametersH()    const
+{
+    TMatrixD    ret(8,1);
+    ret[0][0]   = GMyTrackH_nParameters * TMath::Sin(theta) * TMath::Cos(phi) - vertex.x();
+    ret[1][0]   = GMyTrackH_nParameters * TMath::Sin(theta) * TMath::Sin(phi) - vertex.y();
+    ret[2][0]   = GMyTrackH_nParameters * TMath::Cos(theta)                   - vertex.z();
+    ret[3][0]   = e;
+    ret[4][0]   = vertex.x();
+    ret[5][0]   = vertex.y();
+    ret[6][0]   = vertex.z();
+    ret[7][0]   = mass;
+}
+
+TMatrixD    GMyTrackM::GetCovarianceH()    const
+{
+    TMatrixD    ret(GMyTrackH_nParameters, GMyTrackH_nParameters);
+    Double_t    help    = GMyTrackM_CBRadius*TMath::Cos(theta)*TMath::Cos(phi)*dtheta;
+    ret[0][0]   = help*help;
+    help    = GMyTrackM_CBRadius*TMath::Sin(theta)*TMath::Sin(phi)*dphi;
+    ret[0][0]   += help*help;
+    ret[0][0]   += dvertex.x()*dvertex.x();
+
+    help    = GMyTrackM_CBRadius*TMath::Cos(theta)*dtheta;
+    ret[1][0]   = help*help*TMath::Sin(phi)*TMath::Cos(phi);
+    help    = GMyTrackM_CBRadius*TMath::Sin(theta)*dphi;
+    ret[1][0]   += help*help*TMath::Sin(phi)*TMath::Cos(phi);
+    ret[0][1]   = ret[1][0];
+
+    help    = GMyTrackM_CBRadius*TMath::Cos(theta)*TMath::Sin(phi)*dtheta;
+    ret[1][1]   = help*help;
+    help    = GMyTrackM_CBRadius*TMath::Sin(theta)*TMath::Cos(phi)*dphi;
+    ret[1][1]   += help*help;
+    ret[1][1]   += dvertex.y()*dvertex.y();
+
+    help    = GMyTrackM_CBRadius*dtheta;
+    ret[2][0]   = -help*help*TMath::Sin(theta)*TMath::Cos(theta);
+    ret[0][2]   = ret[2][0];
+
+    help    = GMyTrackM_CBRadius*dtheta;
+    ret[2][1]   = help*help*TMath::Sin(theta)*TMath::Cos(theta);
+    ret[1][2]   = ret[2][1];
+
+    help    = GMyTrackM_CBRadius*TMath::Sin(theta)*dtheta;
+    ret[2][2]   = help*help;
+    ret[2][2]   += dvertex.z()*dvertex.z();
+
+    ret[3][0]   = 0;    ret[0][3]   = 0;
+    ret[3][1]   = 0;    ret[1][3]   = 0;
+    ret[3][2]   = 0;    ret[2][3]   = 0;
+    ret[3][3]   = de*de;
+
+    ret[4][0]   = -dvertex.x()*dvertex.x(); ret[0][4]   = ret[4][0];
+    ret[4][1]   = 0;                ret[1][4]   = 0;
+    ret[4][2]   = 0;                ret[2][4]   = 0;
+    ret[4][3]   = 0;                ret[3][4]   = 0;
+    ret[4][4]   = dvertex.x()*dvertex.x();
+
+    ret[5][0]   = 0;                ret[0][5]   = 0;
+    ret[5][1]   = -dvertex.y()*dvertex.y(); ret[1][5]   = ret[5][1];
+    ret[5][2]   = 0;                ret[2][5]   = 0;
+    ret[5][3]   = 0;                ret[3][5]   = 0;
+    ret[5][4]   = 0;                ret[4][5]   = 0;
+    ret[5][5]   = dvertex.y()*dvertex.y();
+
+    ret[6][0]   = 0;                ret[0][6]   = 0;
+    ret[6][1]   = 0;                ret[1][6]   = 0;
+    ret[6][2]   = -dvertex.z()*dvertex.z(); ret[2][6]   = ret[6][2];
+    ret[6][3]   = 0;                ret[3][6]   = 0;
+    ret[6][4]   = 0;                ret[4][6]   = 0;
+    ret[6][5]   = 0;                ret[5][6]   = 0;
+    ret[6][6]   = dvertex.z()*dvertex.z();
+
+    return ret;
+}
+
+
+
+
+GMyTrackH::GMyTrackH(const GMyTrackM& m):
+    helpVertex(),
+    e(0),
+    mass(0),
+    vertex(),
+    cm(m.GetCovarianceH())
+{
+    TMatrixD    p(m.GetParametersH());
+
+    helpVertex.SetXYZ(p[0][0], p[1][0], p[2][0]);
+    e   = p[3][0];
+    mass= p[7][0];
+    vertex.SetXYZ(p[4][0], p[5][0], p[6][0]);
+}
+
+GMyTrackH::~GMyTrackH()
+{
+
+}
+
+TMatrixD    GMyTrackH::GetParametersW()    const
+{
+    TMatrixD    ret(7,1);
+    Double_t    help    = TMath::Sqrt((e*e)-(mass*mass));
+    help    /= helpVertex.Mag();
+    ret[0][0]   = help * helpVertex.x();
+    ret[1][0]   = help * helpVertex.y();
+    ret[2][0]   = help * helpVertex.z();
+    ret[3][0]   = e;
+    ret[4][0]   = vertex.x();
+    ret[5][0]   = vertex.y();
+    ret[6][0]   = vertex.z();
+}
+
+TMatrixD    GMyTrackH::GetCovarianceW()    const
+{
+    Double_t    help    = TMath::Sqrt((e*e)-(mass*mass));
+    Double_t    r       = helpVertex.Mag();
+
+    TMatrixD    d(7,7);
+    d[0][0] = e * helpVertex.X()/(help*r);
+    d[0][1] = ((helpVertex.Y()*helpVertex.Y())+(helpVertex.z()*helpVertex.z()))*help/(r*r*r);
+    d[0][2] = -helpVertex.x()*helpVertex.y()*help/(r*r*r);
+    d[0][3] = -helpVertex.x()*helpVertex.z()*help/(r*r*r);
+    d[0][4] = 0;
+    d[0][5] = 0;
+    d[0][6] = 0;
+
+    d[1][0] = e * helpVertex.y()/(help*r);
+    d[1][1] = -helpVertex.y()*helpVertex.x()*help/(r*r*r);
+    d[1][2] = ((helpVertex.x()*helpVertex.x())+(helpVertex.z()*helpVertex.z()))*help/(r*r*r);
+    d[1][3] = -helpVertex.y()*helpVertex.z()*help/(r*r*r);
+    d[1][4] = 0;
+    d[1][5] = 0;
+    d[1][6] = 0;
+
+    d[2][0] = e * helpVertex.z()/(help*r);
+    d[2][1] = -helpVertex.z()*helpVertex.x()*help/(r*r*r);
+    d[2][2] = -helpVertex.z()*helpVertex.y()*help/(r*r*r);
+    d[2][3] = ((helpVertex.x()*helpVertex.x())+(helpVertex.y()*helpVertex.y()))*help/(r*r*r);
+    d[2][4] = 0;
+    d[2][5] = 0;
+    d[2][6] = 0;
+
+    d[3][0] = 0;
+    d[3][1] = 0;
+    d[3][2] = 0;
+    d[3][3] = 1;
+    d[3][4] = 0;
+    d[3][5] = 0;
+    d[3][6] = 0;
+
+    d[4][0] = 0;
+    d[4][1] = 0;
+    d[4][2] = 0;
+    d[4][3] = 0;
+    d[4][4] = 1;
+    d[4][5] = 0;
+    d[4][6] = 0;
+
+    d[5][0] = 0;
+    d[5][1] = 0;
+    d[5][2] = 0;
+    d[5][3] = 0;
+    d[5][4] = 0;
+    d[5][5] = 1;
+    d[5][6] = 0;
+
+    d[6][0] = 0;
+    d[6][1] = 0;
+    d[6][2] = 0;
+    d[6][3] = 0;
+    d[6][4] = 0;
+    d[6][5] = 0;
+    d[6][6] = 1;
+
+    TMatrixD    dt(d.Transpose(d));
+
+    return d*cm*dt;
+}
+
+GMyKinFitter::GMyKinFitter(const Int_t npart, const Int_t ncon)    :
+    nPart(npart),
+    nPar(nPart*GMyKinFitter_fNvar),
+    nCon(ncon),
+    fNparti(0),
+    fNpari(0),
+    fNconi(0),
+    fNiter(0),
+    fmAlpha0(nPar,1),
+    fmAlpha(nPar,1),
+    fmV_Alpha0(nPar,nPar),
+    fmV_Alpha(nPar,nPar),
+    fmD(nCon,nPar),
+    fmd(nCon,1),
+    fmlamda(nCon,1),
+    fmV_D(nCon,nCon),
+    fT(nPar,nPar),
+    fPtot(0, 0, 0, 0)
+{
+}
+
+GMyKinFitter::~GMyKinFitter()
+{
+
+}
+
+TMatrixD    GMyKinFitter::ConvertMtoH(const Double_t* pIn, const Double_t* dpIn, Double_t* pOut)
+{
+    TMatrixD    ret(GMyKinFitter_fNvar, GMyKinFitter_fNvar);
+    Double_t    help    = GMyKinFitter_l*TMath::Cos(pIn[1])*TMath::Cos(pIn[2])*dpIn[1];
+    ret[0][0]   = help*help;
+    help    = GMyKinFitter_l*TMath::Sin(pIn[1])*TMath::Sin(pIn[2])*dpIn[2];
+    ret[0][0]   += help*help;
+    ret[0][0]   += dpIn[3]*dpIn[3];
+
+    help    = GMyKinFitter_l*TMath::Cos(pIn[1])*dpIn[1];
+    ret[1][0]   = help*help*TMath::Sin(pIn[2])*TMath::Cos(pIn[2]);
+    help    = GMyKinFitter_l*TMath::Sin(pIn[1])*dpIn[2];
+    ret[1][0]   += help*help*TMath::Sin(pIn[2])*TMath::Cos(pIn[2]);
+    ret[0][1]   = ret[1][0];
+
+    help    = GMyKinFitter_l*TMath::Cos(pIn[1])*TMath::Sin(pIn[2])*dpIn[1];
+    ret[1][1]   = help*help;
+    help    = GMyKinFitter_l*TMath::Sin(pIn[1])*TMath::Cos(pIn[2])*dpIn[2];
+    ret[1][1]   += help*help;
+    ret[1][1]   += dpIn[4]*dpIn[4];
+
+    help    = GMyKinFitter_l*dpIn[1];
+    ret[2][0]   = -help*help*TMath::Sin(pIn[1])*TMath::Cos(pIn[1]);
+    ret[0][2]   = ret[2][0];
+
+    help    = GMyKinFitter_l*dpIn[1];
+    ret[2][1]   = help*help*TMath::Sin(pIn[1])*TMath::Cos(pIn[1]);
+    ret[1][2]   = ret[2][1];
+
+    help    = GMyKinFitter_l*TMath::Sin(pIn[1])*dpIn[1];
+    ret[2][2]   = help*help;
+    ret[2][2]   += dpIn[5]*dpIn[5];
+
+    ret[3][0]   = 0;    ret[0][3]   = 0;
+    ret[3][1]   = 0;    ret[1][3]   = 0;
+    ret[3][2]   = 0;    ret[2][3]   = 0;
+    ret[3][3]   = dpIn[0]*dpIn[0];
+
+    ret[4][0]   = -dpIn[3]*dpIn[3]; ret[0][4]   = ret[4][0];
+    ret[4][1]   = 0;                ret[1][4]   = 0;
+    ret[4][2]   = 0;                ret[2][4]   = 0;
+    ret[4][3]   = 0;                ret[3][4]   = 0;
+    ret[4][4]   = 0;
+
+    ret[5][0]   = 0;                ret[0][5]   = 0;
+    ret[5][1]   = -dpIn[4]*dpIn[4]; ret[1][5]   = ret[5][1];
+    ret[5][2]   = 0;                ret[2][5]   = 0;
+    ret[5][3]   = 0;                ret[3][5]   = 0;
+    ret[5][4]   = 0;                ret[4][5]   = 0;
+    ret[5][5]   = 0;
+
+    ret[6][0]   = 0;                ret[0][6]   = 0;
+    ret[6][1]   = 0;                ret[1][6]   = 0;
+    ret[6][2]   = -dpIn[5]*dpIn[5]; ret[2][6]   = ret[6][2];
+    ret[6][3]   = 0;                ret[3][6]   = 0;
+    ret[6][4]   = 0;                ret[4][6]   = 0;
+    ret[6][5]   = 0;                ret[5][6]   = 0;
+    ret[6][6]   = 0;
+
+    return ret;
+}
+
+TMatrixD    GMyKinFitter::ConvertHtoW(const Double_t* pIn, const TMatrixD& dpIn, const Double_t mass, Double_t* pOut)
+{
+    TMatrixD    ret(GMyKinFitter_fNvar, GMyKinFitter_fNvar);
+    Double_t    sqrtTerm    = TMath::Sqrt((pIn[3]*pIn[3])-(mass*mass));
+    Double_t    r           = TMath::Sqrt((pIn[4]*pIn[4])+(pIn[5]*pIn[5])+(pIn[6]*pIn[6]));
+
+    Double_t    help    = r*r*pIn[3]*pIn[0];
+    ret[0][0]   = help*help*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm*((pIn[1]*pIn[1])+(pIn[2]*pIn[2]));
+    ret[0][0]   += help*help*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm*pIn[1];
+    ret[0][0]   += help*help*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm*pIn[2];
+    ret[0][0]   += help*help*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[0][0]   /= help*help;
+
+    help    = r*r*pIn[3];
+    ret[1][0]   = help*help*pIn[0]*pIn[1]*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm;
+    ret[1][0]   += help*help*((pIn[1]*pIn[1])+(pIn[2]*pIn[2]))*pIn[0]*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm;
+    ret[1][0]   += help*help*((pIn[0]*pIn[0])+(pIn[2]*pIn[2]))*pIn[1]*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm*pIn[2];
+    ret[1][0]   += help*help*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[1][0]   /= help*help;
+    ret[0][1]   = ret[1][0];
+
+    help    = r*r*pIn[3]*pIn[1];
+    ret[1][1]   = help*help*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm*pIn[0];
+    ret[1][1]   += help*help*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm*((pIn[0]*pIn[0])+(pIn[2]*pIn[2]));
+    ret[1][1]   += help*help*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm*pIn[2];
+    ret[1][1]   += help*help*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[1][1]   /= help*help;
+
+    help    = r*r*pIn[3];
+    ret[2][0]   = help*help*pIn[0]*pIn[2]*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm;
+    ret[2][0]   += help*help*((pIn[1]*pIn[1])+(pIn[2]*pIn[2]))*pIn[0]*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm*pIn[1];
+    ret[2][0]   += help*help*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm;
+    ret[2][0]   += help*help*((pIn[0]*pIn[0])+(pIn[1]*pIn[1]))*pIn[2]*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[2][0]   /= help*help;
+    ret[0][2]   = ret[2][0];
+
+    help    = r*r*pIn[3];
+    ret[2][1]   = help*help*pIn[1]*pIn[2]*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm*pIn[0];
+    ret[2][1]   += help*help*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm;
+    ret[2][1]   += help*help*((pIn[0]*pIn[0])+(pIn[2]*pIn[2]))*pIn[1]*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm;
+    ret[2][1]   += help*help*((pIn[0]*pIn[0])+(pIn[1]*pIn[1]))*pIn[2]*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[2][1]   /= help*help;
+    ret[1][2]   = ret[2][1];
+
+    help    = r*r*pIn[3]*pIn[2];
+    ret[2][2]   = help*help*dpIn[3][3];
+    help    = sqrtTerm*sqrtTerm*pIn[0];
+    ret[2][2]   += help*help*dpIn[0][0];
+    help    = sqrtTerm*sqrtTerm*pIn[1];
+    ret[2][2]   += help*help*dpIn[1][1];
+    help    = sqrtTerm*sqrtTerm*((pIn[0]*pIn[0])+(pIn[1]*pIn[1]));
+    ret[2][2]   += help*help*dpIn[2][2];
+    help    = sqrtTerm*r*r*r;
+    ret[2][2]   /= help*help;
+
+
+    ret[3][0]   = r*r*pIn[3]*pIn[0]*dpIn[3][3];
+    ret[3][0]   /= sqrtTerm*r*r*r;
+    ret[0][3]   = ret[3][0];
+
+    ret[3][1]   = r*r*pIn[3]*pIn[1]*dpIn[3][3];
+    ret[3][1]   /= sqrtTerm*r*r*r;
+    ret[1][3]   = ret[3][1];
+
+    ret[3][2]   = r*r*pIn[3]*pIn[2]*dpIn[3][3];
+    ret[3][2]   /= sqrtTerm*r*r*r;
+    ret[2][3]   = ret[3][2];
+
+    ret[3][3]   = dpIn[3][3];
+
+/*
+    ret[4][0]   = -dpIn[3]*dpIn[3]; ret[0][4]   = ret[4][0];
+    ret[4][1]   = 0;                ret[1][4]   = 0;
+    ret[4][2]   = 0;                ret[2][4]   = 0;
+    ret[4][3]   = 0;                ret[3][4]   = 0;
+    ret[4][4]   = 0;
+
+    ret[5][0]   = 0;                ret[0][5]   = 0;
+    ret[5][1]   = -dpIn[4]*dpIn[4]; ret[1][5]   = ret[5][1];
+    ret[5][2]   = 0;                ret[2][5]   = 0;
+    ret[5][3]   = 0;                ret[3][5]   = 0;
+    ret[5][4]   = 0;                ret[4][5]   = 0;
+    ret[5][5]   = 0;
+
+    ret[6][0]   = 0;                ret[0][6]   = 0;
+    ret[6][1]   = 0;                ret[1][6]   = 0;
+    ret[6][2]   = -dpIn[5]*dpIn[5]; ret[2][6]   = ret[6][2];
+    ret[6][3]   = 0;                ret[3][6]   = 0;
+    ret[6][4]   = 0;                ret[4][6]   = 0;
+    ret[6][5]   = 0;             ret[5][6]   = 0;
+    ret[6][6]   = 0;
+*/
+    return ret;
+}
+
+void GMyKinFitter::AddParticle(const Double_t* p, const Double_t* dp)
+{
+    if(fNparti>nPart){
+      std::cout<<"GKinFitter::AddPosKFParticle already at max particles"<<std::endl;
+      return;
+    }
+
+    //Add parameters to Alpha0
+    for(int i=0; i<GMyKinFitter_fNvar; i++)
+        fmAlpha0[fNpari+i][0] = p[i];
+    //Add error matrix to V_Alpha0
+    for(int i=0; i<GMyKinFitter_fNvar; i++)
+        fmAlpha0[fNpari+i][0] = p[i];
+    //fmV_Alpha0.SetSub(fNpari,fNpari,p.GetVAlpha());
+    //Add transformation matrice to fT
+    //fT.SetSub(fNpari,fNpari,p.GetT());
+    //ADD Lorentz Vector !!
+    if(fNparti) fPtot=fPtot+p;
+    else fPtot=p;
+
+    //increment counters
+    fNpari+=GMyKinFitter_fNvar;
+    fNparti++;
+}
+
+void    GMyKinFitter::Reset()
+{
+    fNparti = 0;
+    fNpari  = 0;
+    fNconi  = 0;
+    fNiter  = 0;
+    fPtot.SetPxPyPzE(0, 0, 0, 0);
+}
+
+
